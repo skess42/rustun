d5e37b0293dcbb5bfc39e8bb950c2bb9a567dab6
skess42 <skagaros@gmail.com>
2021-11-30 17:37:45 +0100

    Add quantum secure tunnel encyption using AES and quantum keys

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # rustun
-Simple Linux tunnel ("VPN" but not encrypted/authenticated) in 100 lines of Rust. IPv4. This is a PoC, not an OpenVPN replacement.
+Simple Linux tunnel ("VPN") in Rust. IPv4.
+The tunnel ist AES encrpyted using quantum keys. The keys have to be provided by a REST service running on 127.0.0.1:5000 which is expected to follow the ETSI 014 standard.
+
+This is a PoC, not an OpenVPN replacement.
 
 # Building
 - Clone or download this repository onto a Linux system
@@ -9,11 +12,6 @@ Simple Linux tunnel ("VPN" but not encrypted/authenticated) in 100 lines of Rust
 # Usage
 - Copy rustun to your Linux server and client systems
 - On the server box, run `sudo ./rustun -s`
-- On the client box, run `sudo ./rustun your.server.ip:14284`
+- On the client box, run `sudo ./rustun your.server.ip:3030`
 - You should now have a tunnel. You can verify this by running `ifconfig` on each box
-- Use your tunnel by running `ping 10.8.3.1` on your client. It should ping the server and you should see replies. If not, check firewalls in between.
-
-# Advanced
-Want to route your traffic through the tunnel?
-- On your server run `sudo iptables -t nat -A POSTROUTING -j MASQUERADE ; echo "1" | sudo tee /proc/sys/net/ipv4/ip_forward` to enable forwarding and NAT'ing
-- On your client set up a static /32 route for your target IP (so the VPN packets don't try to get routed through the VPN) then set 10.8.3.1 as your default gateway. Something like this might work, please substitute out $SERVERIP first though: `OLDGW=$(route -n | grep '^0\.0\.0\.0' | awk '{print $2}') ; route add -host $SERVERIP gw $OLDGW ; route add default gw 10.8.3.1`
+- Use your tunnel by running `ping 10.0.5.3` on your client. It should ping the server and you should see replies. If not, check firewalls in between.
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use libc::{open, ioctl, write, read};
+use aes_gcm::aead::{Aead, NewAead};
+use aes_gcm::{Aes256Gcm, Key, Nonce};
+use libc::{ioctl, open, read, write};
+use rand::Rng;
+use serde::{Deserialize, Serialize};
+use std::ffi::CString;
+use std::net::{SocketAddr, UdpSocket};
 use std::os::raw::{c_char, c_short};
 use std::sync::{Arc, Mutex};
-use std::ffi::CString;
-use std::{io, mem, thread, env};
-use std::net::{SocketAddr, UdpSocket};
+use std::{env, io, mem, thread};
 const TUNSETIFF: u64 = 0x400454ca;
 const SIOCGIFFLAGS: u64 = 0x8913;
 const SIOCSIFFLAGS: u64 = 0x8914;
@@ -24,59 +28,102 @@ pub struct SetAddr {
     ifname: [c_char; 16],
     addr: libc::sockaddr_in,
 }
-fn check(ret: i32) -> io::Result<()>{
-    if ret != 0{
+
+#[derive(Deserialize)]
+struct JsonKeys {
+    keys: Vec<JsonKey>,
+}
+
+#[derive(Deserialize)]
+struct JsonKey {
+    #[serde(rename = "key_ID")]
+    key_id: String,
+    key: String,
+}
+
+#[derive(Deserialize, Serialize)]
+struct Message {
+    nonce: [u8; 12],
+    key_id: String,
+    ciphertext: Vec<u8>,
+}
+
+fn check(ret: i32) -> io::Result<()> {
+    if ret != 0 {
         return Err(io::Error::from_raw_os_error(ret));
     }
     Ok(())
 }
-fn main() -> io::Result<()>{
-    let arg = env::args().nth(1).expect("Argument missing! IP:port or -s to run as server");
+fn main() -> io::Result<()> {
+    let arg = env::args()
+        .nth(1)
+        .expect("Argument missing! IP:port or -s to run as server");
     let socket = if arg == "-s" {
-        UdpSocket::bind("0.0.0.0:14284")? //server: bind to 14284
+        UdpSocket::bind("0.0.0.0:3030")? //server: bind to 3030
     } else {
         UdpSocket::bind("0.0.0.0:0")? //client: let system select port to bind to
     };
-    let addr: Arc<Mutex<Option<SocketAddr>>> = if arg == "-s" { //addr will be shared by each thread
+    let addr: Arc<Mutex<Option<SocketAddr>>> = if arg == "-s" {
+        //addr will be shared by each thread
         Arc::new(Mutex::new(None)) //server doesn't know who to talk to yet
     } else {
         Arc::new(Mutex::new(Some(arg.parse().expect("Invalid IP:port")))) //client parses IP:port
     };
-    let fd = unsafe{open(CString::new("/dev/net/tun").unwrap().as_ptr(), 2)};
+    let fd = unsafe { open(CString::new("/dev/net/tun").unwrap().as_ptr(), 2) };
     let mut params = SetIff {
-        ifname: [b't' as i8,b'u' as i8, b'n' as i8, b'7' as i8,0,0,0,0,0,0,0,0,0,0,0,0],//"tun7",
+        ifname: [
+            b't' as i8, b'u' as i8, b'n' as i8, b'1' as i8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        ], //"tun1",
         flags: IFF_TUN | IFF_NO_PI,
         slack: [0; 128],
     };
-    check(unsafe{ioctl(fd, TUNSETIFF, &params)})?; //Set tun flags
-    let s = unsafe{libc::socket(libc::AF_INET, libc::SOCK_DGRAM, libc::IPPROTO_IP)}; //ioctl socket
-    let last_octet = if arg == "-s" { 1 } else { 2 }; // server is 10.8.3.1 client is 10.8.3.2
+    check(unsafe { ioctl(fd, TUNSETIFF, &params) })?; //Set tun flags
+    let s = unsafe { libc::socket(libc::AF_INET, libc::SOCK_DGRAM, libc::IPPROTO_IP) }; //ioctl socket
+    let last_octet = if arg == "-s" { 3 } else { 2 }; // server is 10.0.5.3 client is 10.0.5.2
     let mut tun_addr = SetAddr {
         ifname: params.ifname.clone(), //same interface
-        addr: libc::sockaddr_in{
+        addr: libc::sockaddr_in {
             sin_family: libc::AF_INET as u16,
             sin_port: 0,
-            sin_addr: libc::in_addr{s_addr: u32::from_ne_bytes([10,8,3,last_octet])}, //10.8.3.X
+            sin_addr: libc::in_addr {
+                s_addr: u32::from_ne_bytes([10, 0, 5, last_octet]),
+            }, //10.0.5.X
             sin_zero: [0; 8],
-        }
+        },
     };
-    check(unsafe{ioctl(s, SIOCSIFADDR, &tun_addr)})?; //Set address of interface (10.8.3.X)
-    check(unsafe{ioctl(s, SIOCGIFFLAGS, &params)})?; //get flags
+    check(unsafe { ioctl(s, SIOCSIFADDR, &tun_addr) })?; //Set address of interface (10.0.5.X)
+    check(unsafe { ioctl(s, SIOCGIFFLAGS, &params) })?; //get flags
     params.flags = params.flags | IFF_RUNNING | IFF_UP; //add UP+RUNNING to flags
-    check(unsafe{ioctl(s, SIOCSIFFLAGS, &params)})?; //set flags (turns interface on)
-    tun_addr.addr.sin_addr.s_addr = u32::from_ne_bytes([255,255,255,0]);
-    check(unsafe{ioctl(s, SIOCSIFNETMASK, &tun_addr)})?; //set netmask (255.255.255.0)
+    check(unsafe { ioctl(s, SIOCSIFFLAGS, &params) })?; //set flags (turns interface on)
+    tun_addr.addr.sin_addr.s_addr = u32::from_ne_bytes([255, 255, 255, 0]);
+    check(unsafe { ioctl(s, SIOCSIFNETMASK, &tun_addr) })?; //set netmask (255.255.255.0)
     let addr_ref = Arc::clone(&addr); //reference to addr for recv thread to update
     let rcv_sock = socket.try_clone()?; //clone socket handle for thread to use
-    thread::spawn(move || { //this thread forwards from socket to tun device
+    let arg_cpy = arg.clone();
+
+    thread::spawn(move || {
+        //this thread forwards from socket to tun device
         let mut buf = [0; 65536]; //support full jumbo frames
-        while let Ok((rcvd, src_addr)) = rcv_sock.recv_from(&mut buf){
-            let _ = addr_ref.lock().and_then(|mut a|Ok(a.replace(src_addr))); //save addr
-            (&mut buf[..rcvd]).iter_mut().map(|b| *b ^= 0xff).last(); //Decrypt!
-            let origptr: *const u8 = &buf[0]; //we'll need transmute to convert *const u8 to void*
-            let res = unsafe{write(fd, mem::transmute(origptr), rcvd)}; //send it along
-            if res != rcvd as isize{
-                eprintln!("Error? {} vs expected {}", res, rcvd);
+        while let Ok((rcvd, src_addr)) = rcv_sock.recv_from(&mut buf) {
+            let _ = addr_ref.lock().and_then(|mut a| Ok(a.replace(src_addr))); //save addr
+
+            let message: Message = bincode::deserialize(&buf[..rcvd]).unwrap();
+            println!("Recieved encrypted packet: {:x?}", message.ciphertext);
+
+            //use matching qkd-key for decryption
+            let key = lookup_key(&arg_cpy, message.key_id).unwrap();
+            let key = Key::from_slice(&key[0..32]);
+            let cipher = Aes256Gcm::new(key);
+
+            let nonce = Nonce::from_slice(&message.nonce);
+            let plaintext = cipher.decrypt(nonce, message.ciphertext.as_ref()).unwrap();
+            println!("Send plaintext packet on tun1: {:?}", &plaintext);
+
+            let plaintext_len = plaintext.len();
+            let origptr: *const u8 = &plaintext[0]; //we'll need transmute to convert *const u8 to void*
+            let res = unsafe { write(fd, mem::transmute(origptr), plaintext_len) }; //send it along
+            if res != plaintext_len as isize {
+                eprintln!("Error? {} vs expected {}", res, plaintext_len);
                 std::process::exit(1) //exit if write fails
             }
         }
@@ -84,17 +131,72 @@ fn main() -> io::Result<()>{
     let mut buf = [0; 65536]; //support full jumbo frames
     let buf_u8_ptr: *mut u8 = &mut buf[0]; //we'll need transmute to convert *mut u8 to void*
     loop {
-        let read_res = unsafe{read(fd, mem::transmute(buf_u8_ptr), 65536)};
+        let read_res = unsafe { read(fd, mem::transmute(buf_u8_ptr), 65536) };
         if read_res <= 0 {
             eprintln!("Error? read {}", read_res);
             break;
         }
-        (&mut buf[..read_res as usize]).iter_mut().map(|b| *b ^= 0xff).last(); //Encrypt!
+
+        let message = &buf[..read_res as usize];
+        println!("Recieved packet on tun1: {:?}", message);
+
+        // recieve new key from qkd-device
+        let client = if arg == "-s" { "ETSIA" } else { "ETSIB" };
+        let url = format!("https://127.0.0.1:5000/api/v1/keys/{}/enc_keys", client);
+        let client = reqwest::blocking::Client::builder()
+            .danger_accept_invalid_certs(true)
+            .build()
+            .unwrap();
+        let response = client.get(&url).send().unwrap().text().unwrap();
+        println!("Get QKD key: {}", response);
+
+        let mut response: JsonKeys = serde_json::from_str(&response).unwrap();
+        let JsonKey { key_id, key } = response.keys.pop().unwrap();
+        let qkd_key = key.as_bytes();
+
+        //use qkd-key for encryption
+        let cipher = Aes256Gcm::new(Key::from_slice(&qkd_key[0..32]));
+        let nonce = rand::thread_rng().gen::<[u8; 12]>(); // unique nonce per message
+        let ciphertext = cipher
+            .encrypt(Nonce::from_slice(&nonce), message.as_ref())
+            .unwrap();
+        println!("Encrypted packet: {:x?}", &ciphertext);
+
+        let encrypted_message = Message {
+            nonce,
+            key_id,
+            ciphertext,
+        };
+        let encrypted_message = bincode::serialize(&encrypted_message).unwrap();
+
         if let Ok(addr_guard) = addr.lock() {
-            if let Some(addr_real) = *addr_guard { //server sends to last seen addr
-                socket.send_to(&buf[0..read_res as usize], addr_real)?;
+            if let Some(addr_real) = *addr_guard {
+                //server sends to last seen addr
+                socket.send_to(&encrypted_message, addr_real)?;
             }
         }
     }
     Ok(())
+}
+
+fn lookup_key(arg: &str, key_id: String) -> io::Result<Vec<u8>> {
+    let client = if arg == "-s" { "ETSIA" } else { "ETSIB" };
+    let url = format!("https://127.0.0.1:5000/api/v1/keys/{}/dec_keys", client);
+    let body = format!(r#"{{"key_IDs":[{{"key_ID":"{}"}}]}}"#, key_id);
+    let client = reqwest::blocking::Client::builder()
+        .danger_accept_invalid_certs(true)
+        .build()
+        .unwrap();
+    let response = client
+        .post(&url)
+        .header("Content-Type", "application/json")
+        .body(body)
+        .send()
+        .unwrap()
+        .text()
+        .unwrap();
+    println!("Lookup QKD key response: {}", response);
+    let mut answer: JsonKeys = serde_json::from_str(&response).unwrap();
+    let JsonKey { key_id: _, key } = answer.keys.pop().unwrap();
+    Ok(key.as_bytes().to_vec())
 }
